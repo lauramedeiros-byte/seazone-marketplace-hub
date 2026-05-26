b4b5ffbe1c20b1219b6472b5b1197177f9f3b41a
Laura Medeiros <laura.medeiros@seazone.com.br>
2026-05-26 10:40:12 -0300

    fix: remove earlyAccess do prisma config

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -2,6 +2,5 @@ import path from "node:path";
 import type { PrismaConfig } from "prisma";
 
 export default {
-  earlyAccess: true,
   schema: path.join("prisma", "schema.prisma"),
 } satisfies PrismaConfig;
